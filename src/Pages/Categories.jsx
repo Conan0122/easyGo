ad12693c84acbbcd2977f631cd3808777766a8c2
Anish <anishkgouda@gmail.com>
2024-10-05 15:23:37 +0530

    test

--- src/Pages/Categories.jsx
+++ src/Pages/Categories.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Categories() {
   return (
     <div className='w-full h-screen flex justify-center items-center'>
-      <h1>Categories</h1>
+      <h1>Categories hbfdvbidsi</h1>
     </div>
   )
 }
